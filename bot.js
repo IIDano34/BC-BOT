6b3c43f48e40e9704aa42c8d60d65811246f7bae
IIDano34 <45509559+IIDano34@users.noreply.github.com>
2019-04-15 14:09:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,34 +219,5 @@ var prefix = "!"
   
   });
 
-    if(message.content === prefix + "uc") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: true
-
-              }).then(() => {
-                  message.reply("**__تم فتح الشات__✅**")
-              });
-    }
-       
-});
-
-client.on('message', message => {
-var prefix = "!";
-       if(message.content === prefix + "mc") {
-                           if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: false
-
-              }).then(() => {
-                  message.reply("**__تم تقفيل الشات__ ✅ **")
-              });
-                }
-	
-
 
  

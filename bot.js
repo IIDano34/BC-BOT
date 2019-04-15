9b4ce49cc7767335d64d0cbe8ba4b3d617f4e87d
IIDano34 <45509559+IIDano34@users.noreply.github.com>
2019-04-15 14:04:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -247,22 +247,6 @@ var prefix = "!";
               });
                 }
 	
-	  client.on('message',async message => {
-    if(message.content.startsWith(prefix + "ocount")) {
-    if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply('❌ **ليس لديك الصلاحيات الكافية**');
-    if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply('❌ **ليس معي الصلاحيات الكافية**');
-    message.channel.send('✅| **تم عمل الروم بنجاح**');
-    message.guild.createChannel(`👥Members Count : [ ${message.guild.members.size} ]` , 'voice').then(c => {
-      console.log(`Count Members channel setup for guild: \n ${message.guild.name}`);
-      c.overwritePermissions(message.guild.id, {
-        CONNECT: false,
-        SPEAK: false
-      });
-      setInterval(function() {
-        c.setName(`👥 Members: ${message.guild.members.size} `)
-      },1000);
-    });
-    }
-  });
+
 
  
